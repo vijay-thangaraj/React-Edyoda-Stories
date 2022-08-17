c1abb9b1491e4c23137a2726d01396e41244d8bf
vijay-thangaraj <105139962+vijay-thangaraj@users.noreply.github.com>
2022-08-16 21:46:27 -1200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./Components/Header/Header";
+import Cards from "./Components/Cards/Cards";
+import Footer from "./Components/Footer/Footer";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Cards />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
